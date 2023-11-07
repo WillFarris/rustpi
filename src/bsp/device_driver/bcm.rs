9826ba70dbc8f72d7dd4a89e580a4f8efcf2f12f
Will Farris <9190155+WillFarris@users.noreply.github.com>
2023-11-06 22:09:59 -0800

    start working on SPI driver

--- src/bsp/device_driver/bcm.rs
+++ src/bsp/device_driver/bcm.rs
@@ -2,8 +2,10 @@ mod bcm2xxx_gpio;
 mod bcm2837_mini_uart;
 mod bcm2xxx_qa7;
 mod bcm2xxx_systimer;
+mod bcm2837_spi;
 
 pub use bcm2xxx_gpio::*;
 pub use bcm2837_mini_uart::*;
 pub use bcm2xxx_qa7::*;
 pub use bcm2xxx_systimer::*;
+pub use bcm2837_spi::*;
